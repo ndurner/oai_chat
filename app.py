3214c780e696a2ad704356c9ee11d89116e8da92
Nils Durner <ndurner@googlemail.com>
2025-07-13 12:19:49 +0200

    o3-low support

--- app.py
+++ app.py
@@ -350,11 +350,12 @@ def bot(message, history, oai_key, system_prompt, temperature, max_tokens, model
                 print(f"br_prompt: {str(history_openai_format)}")
 
             if model in ["o1", "o1-high", "o1-pro", "o1-2024-12-17", "o3-mini", "o3-mini-high", "o4-mini", "o4-mini-high",
-                         "o3", "o3-high"]:
+                         "o3", "o3-high", "o3-low"]:
                 reasoner = True
 
                 # reasoning effort
                 high = False
+                low = False
                 if model == "o1-high":
                     model = "o1"
                     high = True
@@ -370,6 +371,9 @@ def bot(message, history, oai_key, system_prompt, temperature, max_tokens, model
                 elif model == "o3-high":
                     model = "o3"
                     high = True
+                elif model == "o3-low":
+                    model = "o3"
+                    low = True
             else:
                 reasoner = False
 
@@ -385,7 +389,12 @@ def bot(message, history, oai_key, system_prompt, temperature, max_tokens, model
                     "store": False
                 }
                 if reasoner:
-                    request_params["reasoning"] = {"effort": "high" if high else "medium"}
+                    if high:
+                        request_params["reasoning"] = {"effort": "high"}
+                    elif low:
+                        request_params["reasoning"] = {"effort": "low"}
+                    else:
+                        request_params["reasoning"] = {"effort": "medium"}
                 else:
                     request_params["temperature"] = temperature
                 if tools:
@@ -612,7 +621,7 @@ with gr.Blocks(delete_cache=(86400, 86400)) as demo:
 
         oai_key = gr.Textbox(label="OpenAI API Key", elem_id="oai_key", value=load_openai_api_key())
         model = gr.Dropdown(label="Model", value="gpt-4.1", allow_custom_value=True, elem_id="model",
-                            choices=["gpt-4o", "gpt-4.1", "gpt-4.5-preview", "o3", "o3-high", "o1-pro", "o1-high", "o1-mini", "o1", "o3-mini-high", "o3-mini", "o4-mini", "o4-mini-high", "chatgpt-4o-latest", "gpt-4o-mini", "gpt-4-turbo", "gpt-4", "whisper", "gpt-image-1"])
+                            choices=["gpt-4o", "gpt-4.1", "gpt-4.5-preview", "o3", "o3-high", "o3-low", "o1-pro", "o1-high", "o1-mini", "o1", "o3-mini-high", "o3-mini", "o4-mini", "o4-mini-high", "chatgpt-4o-latest", "gpt-4o-mini", "gpt-4-turbo", "gpt-4", "whisper", "gpt-image-1"])
         system_prompt = gr.TextArea("You are a helpful yet diligent AI assistant. Answer faithfully and factually correct. Respond with 'I do not know' if uncertain.", label="System/Developer Prompt", lines=3, max_lines=250, elem_id="system_prompt")  
         temp = gr.Slider(0, 2, label="Temperature", elem_id="temp", value=1)
         max_tokens = gr.Slider(0, 16384, label="Max. Tokens", elem_id="max_tokens", value=0)
